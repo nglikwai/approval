e4509f0768d719289ba6f63c683e23d3916af571
LIK WAI NG <nglikwai@ymail.com>
2022-10-12 13:19:23 +0800

    init

---
+++ components/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="menu">
+    <div class="menu-item">HOME</div>
+    <div class="menu-item">APPLICATION</div>
+    <div class="menu-item">IN PROGRESS</div>
+    <div class="menu-item">HISTORY</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sidebar",
+};
+</script>
+
+<style lang="scss" scoped>
+.menu {
+  background-color: #fcfcfc;
+  box-shadow: 0 0 8px #ccc;
+  height: 100vh;
+  padding: 30px;
+}
+
+.menu-item {
+  padding: 20px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <SideBar />
+  </div>
+</template>
+
+<script>
+import SideBar from "../components/SideBar.vue";
+export default {
+  name: "IndexPage",
+  components: { SideBar },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
